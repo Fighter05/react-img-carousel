dbc2e45c543f6be1b6245f89245d73132a4534ec
fighter05 <sitthipon.sint@gmail.com>
2023-01-11 21:10:56 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='flex items-center justify-center h-screen border-red-400 border-solid border-2'>
+      <p className='text-5xl font-bold text-red-400'>Hello,Yayyyyyyyyyyyy!</p>
+    </div>
+  );
+}
+
+export default App;
